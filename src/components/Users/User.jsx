3d14b9b77d64300b3f1c2f469b8955b8e6c79c9a
DivyanshSareen <sareendivyansh@gmail.com>
2022-07-16 17:12:47 +0530

    removed follow option from recommend section

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -1,11 +1,6 @@
-import { useDispatch } from "react-redux";
-import { followUser } from "../../store/users/usersSlice";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 
 const User = ({ user }) => {
-  const dispatch = useDispatch();
   return (
     <Link to={"user/" + user._id} className='rec-user'>
       <div className='avatar'>
@@ -18,17 +13,6 @@ const User = ({ user }) => {
           {user?.firstName} {user?.lastName}
         </p>
         <p className='recc-user--name'>@{user?.username}</p>
-      </div>
-      <div className='rec-options'>
-        <button className='btn btn-ghost'>
-          <FontAwesomeIcon
-            className='sidebar-logo--icon'
-            icon={faUserPlus}
-            onClick={() => {
-              dispatch(followUser(user._id));
-            }}
-          />
-        </button>
       </div>
     </Link>
   );
